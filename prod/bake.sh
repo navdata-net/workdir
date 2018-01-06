7941bbf08f6f87efbe84c27ee27d019b52e80ac6
root <root@valle.smartpal.de>
2018-01-06 21:42:51 +0100

    Update to current build

--- prod/bake.sh
+++ prod/bake.sh
@@ -2,9 +2,12 @@
 pushd . >/dev/null
 . ./oe-init-build-env ${1}
 pwd
-bitbake rpi-basic-image
-bitbake rtklibexplorer
-bitbake pylongps
+bitbake rpi-basic-image parted ntp nginx rrdtool pylongps rtklibexplorer
+#bitbake ntp
+#bitbake nginx
+#bitbake rrdtool
+#bitbake pylongps
+#bitbake rtklibexplorer
 bitbake package-index
 popd >/dev/null
 
